dd83d702184cebe77b72c7ff6d859cbcab92c87d
Hannah Zulueta <hannahjoy.zulueta@gmail.com>
2018-06-23 22:40:36 -0700

    Add routes and controllers

--- server.js
+++ server.js
@@ -11,6 +11,37 @@ app.set('view engine', 'ejs');
 
 let db;
 
+app.get('/', (req, res) => {
+  db.collection(process.env.DB_COLLECTION)
+    .find()
+    .toArray((err, result) => {
+      if (err) return console.log(err);
+      res.render('index.ejs', { questions: result });
+    });
+});
+
+app.post('/questions', (req, res) => {
+  db.collection(process.env.DB_COLLECTION).insertOne(
+    req.body,
+    (err, result) => {
+      if (err) return console.log(err);
+      res.redirect('/');
+    }
+  );
+});
+
+app.put('/questions/:id', (req, res) => {
+  db.collection(process.env.DB_COLLECTION)
+    .updateOne({ _id: ObjectId(req.params.id) }, { $set: req.body })
+    .then(() => res.sendStatus(200));
+});
+
+app.delete('/questions', (req, res) => {
+  db.collection(process.env.DB_COLLECTION)
+    .deleteOne({ _id: ObjectId(req.body.id) })
+    .then(() => res.sendStatus(200));
+});
+
 MongoClient.connect(
   process.env.DB_URL,
   (err, client) => {
