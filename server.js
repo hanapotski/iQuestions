4d6bb7d611767a99613d45ffd6d1822e1d5b0e51
Hannah Zulueta <hannahjoy.zulueta@gmail.com>
2018-06-23 22:37:01 -0700

    Add more config to server.js

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const app = express();
 const bodyParser = require('body-parser');
 const { MongClient, ObjectId } = require('mongodb');
 
+dotenv.config();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-dotenv.config();
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 let db;
 
@@ -14,7 +16,7 @@ MongoClient.connect(
   (err, client) => {
     if (err) return console.log(err);
     db = client.db(process.env.DB_NAME);
-    app.listen(3000, () => {
+    app.listen(process.env.PORT, () => {
       console.log('Server is listening on port 3000');
     });
   }
