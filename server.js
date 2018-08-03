bdaa6a15752ec8377f50f43723fc6952db9261de
Hannah Zulueta <hannahjoy.zulueta@gmail.com.com>
2018-08-03 09:28:01 -0700

    Install cookie-parser

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express'),
   app = express(),
   bodyParser = require('body-parser'),
+  cookieParser = require('cookie-parser'),
   { MongoClient, ObjectId } = require('mongodb'),
   dotenv = require('dotenv'),
   passport = require('passport'),
