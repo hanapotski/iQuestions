a8fcf14e9d9275d2bcc179fa8f789e6fd2a61bcc
Hannah Zulueta <hannahjoy.zulueta@gmail.com>
2018-08-05 16:42:16 -0700

    Add author to question model

--- models/question.js
+++ models/question.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const questionSchema = new mongoose.Schema({
-  question: String
+  question: String,
+  author: String
 });
 
 const question = mongoose.model('question', questionSchema);
