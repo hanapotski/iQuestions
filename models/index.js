1a96875118fe12dc297761b3f862e39e2f214e61
Hannah Zulueta <hannahjoy.zulueta@gmail.com>
2018-08-09 11:26:59 -0700

    Use local db if no env

--- models/index.js
+++ models/index.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose'),
 mongoose.set('debug', true);
 
 dotenv.config();
+
+const db = process.env.DB_URL || 'mongodb://localhost:27017/iquestions';
+
 mongoose.connect(
-  process.env.DB_URL,
+  db,
   { useNewUrlParser: true }
 );
 
